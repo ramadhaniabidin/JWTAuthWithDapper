10e06759e1882eb430e75ebeb3c9f1c79eb77c03
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-18 00:13:19 +0700

    add interface IUserRepository

---
+++ JWTAuthWithDapper/Interface/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using JWTAuthWithDapper.Models;
+
+namespace JWTAuthWithDapper.Interface
+{
+    public interface IUserRepository
+    {
+        public List<UserInfo> SelectUser(string email, string password);
+    }
+}
