882c6a50f0c1911fefcce2981d9d9c9c65f8fc4d
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-20 00:55:15 +0700

    add method for decoding token to payload(claims)

--- JWTAuthWithDapper/Controllers/TokenController.cs
+++ JWTAuthWithDapper/Controllers/TokenController.cs
@@ -5,10 +5,12 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
 
 namespace JWTAuthWithDapper.Controllers
 {
-    [Route("api/token")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class TokenController : Controller
     {
@@ -26,7 +28,37 @@ namespace JWTAuthWithDapper.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post(string email, string password)
+        public IActionResult DocodeToken(string token)
+        {
+            bool falseCondition = (string.IsNullOrWhiteSpace(token));
+            if(!falseCondition)
+            {
+                TokenValidationParameters validationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = "https://localhost:7112",
+                    ValidAudience = "https://localhost:7112",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]))
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                ClaimsPrincipal claimsPrincipal = tokenHandler.ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+
+                var claims = claimsPrincipal.Claims;
+                var email = claims.FirstOrDefault(c => c.Type == "Email")?.Value;
+
+                return Ok(claims);
+            }
+
+            else 
+            { 
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult GetToken(string email, string password)
         {
             
             bool falseCondition = (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password));
@@ -37,22 +69,20 @@ namespace JWTAuthWithDapper.Controllers
                 {
                     var claims = new[]
                     {
-                        //new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
-                        //new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                        //new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
-                        //new Claim("UserId", user.UserId.ToString()),
-                        //new Claim("DisplayName", user.DisplayName),
                         new Claim("Password", password),
                         new Claim("Email", email),
                         new Claim("Role", "Admin")
                     };
 
                     var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    //var psw = claims.FirstOrDefault(c => c.Type == "Email")?.Value;
+
                     var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
                     var token = new JwtSecurityToken(
                         Issuer,
                         Audience,
                         claims, expires: DateTime.UtcNow.AddMinutes(10), signingCredentials: signIn);
+                    var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
 
                     return Ok(new JwtSecurityTokenHandler().WriteToken(token));
                 }
